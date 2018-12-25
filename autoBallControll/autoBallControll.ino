21026e003ea09033a6e46bbecb14ea825ed4f7bb
meguko2002 <meguko2002@gmail.com>
2018-12-25 16:19:34 +0900

    Update autoBallControll.ino

--- autoBallControll/autoBallControll.ino
+++ autoBallControll/autoBallControll.ino
@@ -2,29 +2,26 @@
   autoBallControll !!
  ******************************/
 #include <Servo.h>           //Servo motor
-#include <SNESpaduino.h>     //Super Famicon controller
-
 #define SERVO_L 5
 #define SERVO_R 6
-#define LATCH 7     //orange line 
-#define DAT 8       //red line 
-#define CLOCK 9     //yellow line 
+const float Ak = -0.05;
+const float Bk = 0;
+const float Ck = 0.6;
+const float obj_x = 100;
+
 const int CTR = 90;      //center angle
 const int MAX = 140;     //斜めの上限角度
 const int MIN = 40;      //斜めの下限角度
-const int SQMAX = CTR + 0.7 * (MAX - CTR); //上下左右の振れ角度は斜め振れの0.7倍
-const int SQMIN = CTR - 0.7 * (CTR - MIN);
 const int adj_l = 2, adj_r = -12;    //モータ角度微調整
-const int MOTOR_DELAY = 5;   // MOTOR_DELAY > MotorSpeed (=2.67msec/deg)
-
+const int DELAY = 5;   // DELAY > MotorSpeed (=2.67msec/deg)
+const int OFFSET = 10; //deg
 Servo servo_l;
 Servo servo_r;
-SNESpaduino pad(LATCH, CLOCK, DAT);
 
-void setup(){
+void setup() {
   servo_l.attach(SERVO_L);
   servo_r.attach(SERVO_R);
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 int gopos(int pos, int obj) {
@@ -34,62 +31,31 @@ int gopos(int pos, int obj) {
 }
 
 void loop() {
-  static int pos_l = CTR, pos_r = CTR;
+  int pos_r = CTR + OFFSET, pos_l = CTR - OFFSET;
+  float x = 0, pre_x = 0;
+  float v, pre_v = 0, a, obj_r, obj_l;
+  servo_r.write(pos_r + adj_r);
+  servo_l.write(180 - (pos_l + adj_l));
+  delay(2000);
 
-  int btns = pad.getButtons(false);  //コントローラの入力を読み取る
-
-  if (!(btns & BTN_UP)) {
-    if (!(btns & (BTN_UP | BTN_LEFT))) {
-      pos_l = gopos(pos_l, MIN);
-      pos_r = gopos(pos_r, CTR);
-      Serial.println(pos_l);
-    }
-    else if (!(btns & (BTN_UP | BTN_RIGHT))) {
-      pos_l = gopos(pos_l, CTR);
-      pos_r = gopos(pos_r, MIN);
-    }
-    else {
-      pos_l = gopos(pos_l, SQMIN);
-      pos_r = gopos(pos_r, SQMIN);
-      Serial.println(pos_l);
-    }
+  while (1) {
+    //    x = Serial.read();
+    v = x - pre_x;
+    a = v - pre_v;
+    obj_r = Ak * a + Bk * v + Ck * (x - obj_x) + CTR;
+    obj_l = 180 - obj_r;
+    pos_r = gopos(pos_r, int(obj_r));
+    pos_l = gopos(pos_l, int(obj_l));
+    servo_r.write(pos_r + adj_r);
+    servo_l.write(180 - (pos_l + adj_l));
+    Serial.print("obj_r=");
+    Serial.print(obj_r);
+    Serial.print(",");
+    Serial.print(pos_r);
+    Serial.print(",");
+    Serial.println(pos_l);
+    pre_x = x;
+    pre_v = v;
+    delay(DELAY);
   }
-  else if (!(btns & BTN_DOWN)) {
-    if (!(btns & (BTN_LEFT | BTN_DOWN))) {
-      pos_l = gopos(pos_l, CTR);
-      pos_r = gopos(pos_r, MAX);
-    }
-    else if (!(btns & (BTN_DOWN | BTN_RIGHT))) {
-      pos_l = gopos(pos_l, MAX);
-      pos_r = gopos(pos_r, CTR);
-    }
-    else {
-      pos_l = gopos(pos_l, SQMAX);
-      pos_r = gopos(pos_r, SQMAX);
-    }
-  }
-  else if (!(btns & BTN_LEFT)) {
-    pos_l = gopos(pos_l, SQMIN);
-    pos_r = gopos(pos_r, SQMAX);
-  }
-  else if (!(btns & BTN_RIGHT)) {
-    pos_l = gopos(pos_l, SQMAX);
-    pos_r = gopos(pos_r, SQMIN);
-  }
-
-  else if (!(btns & BTN_SELECT)) {
-    pos_l = CTR;
-    pos_r = CTR;
-  }
-
-//  servo_l.write(pos_l+adj_l);         //move motors
-//  servo_r.write(180 - (pos_r+adj_r)); //サーボは左右で取り付けが逆なので180に対する補数を入力
-  servo_l.write(180-(pos_l+adj_l));         //move motors
-  servo_r.write(pos_r+adj_r); //サーボは左右で取り付けが逆なので180に対する補数を入力
- Serial.print(pos_l);
-  Serial.print(" , ");
- Serial.println(pos_r);
-
-  delay(MOTOR_DELAY);
 }
-
