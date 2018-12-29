5447ccc61234918cee4872f97295d3d3460e5754
meguko2002 <meguko2002@gmail.com>
2018-12-29 18:43:57 +0900

    各処理に要する時間を測定

--- webcam.py
+++ webcam.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
-import pythonToArduino as pa
 
-def color_pick(hue, image):
+
+# import pythonToArduino as pa
+
+def color_pick(hue: object, image: object) -> object:
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     hue += 180
     hue %= 180
@@ -17,6 +19,7 @@ def color_pick(hue, image):
         mask = cv2.inRange(hsv, 0, hsv_max)
         mask += cv2.inRange(hsv, hsv_min, 180)
     return mask
+
 
 def detect_center_of_gravity(cnt):
     try:
@@ -35,39 +38,47 @@ def nothing():
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 # cv2.resizeWindow('image', 500,300)
-
 # Create trackbars for color change
 cv2.createTrackbar('Hue', 'image', 0, 180, nothing)
 
 cap = cv2.VideoCapture(0)
 kernel = np.ones((5, 5), np.uint8)
-while (True):
-    k = cv2.waitKey(1 & 0xFF)
-    if k == 27:
-        break
-    hue = cv2.getTrackbarPos('Hue', 'image')
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    mask = color_pick(hue, frame)
-    # cv2.imshow('mask', mask)
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    # cv2.imshow('opening', opening)
-    image, contours, hierarchy = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+while True:
+    timeave = 0
+    for i in range(10):
+        k = cv2.waitKey(1 & 0xFF)
+        if k == 27:
+            break
+        hue = cv2.getTrackbarPos('Hue', 'image')
+        # Capture frame-by-
+        e1 = cv2.getTickCount()
 
-    contours.sort(key=cv2.contourArea, reverse=True)
-    cv2.putText(frame, 'count =' + str(len(contours)), (0,20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1,
-                cv2.LINE_AA)
-    if len(contours) > 0:
-        cv2.drawContours(frame, [contours[0]], 0, (0, 255, 0), 3)
-        cx, cy = detect_center_of_gravity(contours[0])
-        cv2.putText(frame, " (" + str(cx) + "," + str(cy) + ")", (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
-                    (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.circle(frame, (cx, cy), 5, (0, 0, 0), -1)
-        x = cx
-    cv2.imshow('image', frame)
-    pa.sendtoArd(x)
-    print(x)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # 通しで100msec
+        ret, frame = cap.read()  # 15msec
+        mask = color_pick(hue, frame)  # 10msec
+        # cv2.imshow('mask', mask)
+        opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)  # 1msec
+        cv2.imshow('opening', opening)
+        image, contours, hierarchy = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 1msec
+        contours.sort(key=cv2.contourArea, reverse=True)  # 0msec
+        cv2.putText(frame, 'count =' + str(len(contours)), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1,
+                    cv2.LINE_AA)  # 0msec
+        if len(contours) > 0:  # 1msec
+            cv2.drawContours(frame, [contours[0]], 0, (0, 255, 0), 3)
+            cx, cy = detect_center_of_gravity(contours[0])
+            cv2.putText(frame, " (" + str(cx) + "," + str(cy) + ")", (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
+                        (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.circle(frame, (cx, cy), 5, (0, 0, 0), -1)
+            x = cx
+        # cv2.imshow('image', frame)  # 65msec
+        e2 = cv2.getTickCount()
+        # pa.sendtoArd(x)
+        # print(x)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        time = (e2 - e1) / cv2.getTickFrequency()
+        timeave = (timeave * i + time) / (i + 1)
+    print(timeave)
 cap.release()
 cv2.destroyAllWindows()
